c96ff7d850e2bf2fa7c1ec00f7d199dbf1770068
Sierradia <sophie.bouhin@hei.yncrea.fr>
2019-11-06 17:15:16 -0500

    FEAT (Resize) : possibilité de changer la taille des images

--- app/src/main/java/com/example/tpandroid/MainActivity.java
+++ app/src/main/java/com/example/tpandroid/MainActivity.java
@@ -2,16 +2,12 @@ package com.example.tpandroid;
 
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,57 +26,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         getAllShownImagesPath();
-        setContentView(new MyView(this));
+        setContentView(new Touch(this,imageList));
 
     }
 
-    public class MyView extends View {
 
-        public MyView(Context context) {
-            super(context);
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas){
-            super.onDraw(canvas);
-
-            int largeurPhotos = 3;
-            int compteur = 0;
-            int i=0,j=0;
-            int widthPhone = getWidth();
-
-            if (imageList != null) {
-                while (compteur < imageList.size() && compteur < 10) {
-
-                    Drawable d = Drawable.createFromPath(imageList.get(i * 3 + j));
-                    d.setBounds(widthPhone * j / 3, widthPhone * i / 3, widthPhone * (j + 1) / 3, widthPhone * (i + 1) / 3);
-                    d.draw(canvas);
-
-                    j++;
-                    if (j >= largeurPhotos) {
-                        i++;
-                        j = 0;
-                    }
-
-                    compteur++;
-                }
-            }
-
-            /*
-            Drawable d1 = Drawable.createFromPath(imageList.get(0));
-            Drawable d2 = Drawable.createFromPath(imageList.get(1));
-            Drawable d3 = Drawable.createFromPath(imageList.get(2));
-
-            d1.setBounds(0, 0, getWidth()*1/3, getWidth()*1/3);
-            d2.setBounds(getWidth()*1/3, 0, getWidth()*2/3, getWidth()*1/3);
-            d3.setBounds(getWidth()*2/3, 0, getWidth()*3/3, getWidth()*1/3);
-            d1.draw(canvas);
-            d2.draw(canvas);
-            d3.draw(canvas);
-*/
-
-        }
-    }
 
 
     public void getAllShownImagesPath() {
@@ -119,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 cursor.close();
             }
+
 
         } catch (Exception e) {
             e.printStackTrace();
--- app/src/main/java/com/example/tpandroid/Touch.java
+++ app/src/main/java/com/example/tpandroid/Touch.java
@@ -1,26 +1,22 @@
 package com.example.tpandroid;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import java.util.ArrayList;
 
-public class Touch extends View{
-    private static final int MAX_POINTERS = 5;
+public class Touch extends View {
+
     private float mScale = 1f;
     private GestureDetector mGestureDetector;
     private ScaleGestureDetector mScaleGestureDetector;
-
-    private Paint mPaint;
-    private float mFontSize;
-
-    public Touch(Context context) {
-        super(context);
-    }
 
     public class ZoomGesture extends GestureDetector.SimpleOnGestureListener {
         private boolean normal = true;
@@ -28,25 +24,106 @@ public class Touch extends View{
         @Override
         public boolean onDoubleTap(MotionEvent e) {
             mScale = normal ? 3f : 1f;
-            mPaint.setTextSize(mScale*mFontSize);
             normal = !normal;
             invalidate();
             return true;
         }
     }
 
-    public class ScaleGesture extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            mScale *= detector.getScaleFactor();
-            mPaint.setTextSize(mScale*mFontSize);
-            invalidate();
-            return true;
+
+    /////////////////////////////////////////////////////////////////////////////////
+
+
+    private float mScaleBefore = 1f;
+    private int nbCols = 3;
+
+
+    ArrayList<String> imageList;
+
+    public Touch(Context context, ArrayList<String> imageList) {
+
+        super(context);
+        this.imageList = imageList;
+
+        mGestureDetector = new GestureDetector(context, new ZoomGesture());
+        mScaleGestureDetector = new ScaleGestureDetector(context, new ScaleGesture());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int largeurPhotos = 3;
+        int compteur = 0;
+        int widthPhone = getWidth();
+
+        if (mScaleBefore - mScale < 0) {
+            // Agrandir
+            if (nbCols > 1)
+                nbCols--;
+        } else if (mScaleBefore - mScale > 0) {
+            // Retrécir
+            if (nbCols < 7)
+                nbCols++;
+        }
+
+        mScaleBefore = mScale;
+
+        if (imageList != null) {
+
+            int i = 0, j = 0;
+
+            while (compteur < imageList.size() && compteur < 5) {
+
+
+                Drawable d1 = Drawable.createFromPath(imageList.get(i * 3 + j));
+                Bitmap bitmap = ((BitmapDrawable) d1).getBitmap();
+
+                if (bitmap != null) {
+                    Drawable d = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, 800 / nbCols, 800 / nbCols, true));
+
+                    d.setBounds(widthPhone * j / nbCols, widthPhone * i / nbCols, widthPhone * (j + 1) / nbCols, widthPhone * (i + 1) / nbCols);
+                    d.draw(canvas);
+                }
+
+
+                j++;
+                if (j >= largeurPhotos) {
+                    i++;
+                    j = 0;
+                }
+
+                compteur++;
+            }
         }
     }
 
 
+    private boolean alreadyTouched = false;
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+
+        if (alreadyTouched){
+            alreadyTouched = false;
+        }else{
+            mGestureDetector.onTouchEvent(event);
+            mScaleGestureDetector.onTouchEvent(event);
+            alreadyTouched = true;
+        }
+        return true;
+    }
+
+
+    public class ScaleGesture extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScale *= detector.getScaleFactor();
+            Log.d("Coucou", "MScale : " + mScale);
+            invalidate();
+            return true;
+        }
+    }
 }
-
 
