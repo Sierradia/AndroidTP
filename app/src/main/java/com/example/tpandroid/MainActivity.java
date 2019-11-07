79f9a77654bfb192560422f31408f20c38b7d002
Sierradia <sophie.bouhin@hei.yncrea.fr>
2019-11-07 11:43:36 -0500

    FEAT (Commentaires) : ajout de commentaires

--- app/src/main/java/com/example/tpandroid/MainActivity.java
+++ app/src/main/java/com/example/tpandroid/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.tpandroid;
 
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +16,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private static final int PICK_FROM_GALLERY = 1;
-
+    // Liste des liens vers les images
     private ArrayList<String> imageList;
 
     @Override
@@ -27,13 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
         getAllShownImagesPath();
         setContentView(new Touch(this,imageList));
-
     }
 
-
-
-
-    public void getAllShownImagesPath() {
+    /**
+     * getAllShownImagesPath : demande la permission de lire les images si ce n'est pas déjà fait
+     */
+    private void getAllShownImagesPath() {
 
         try {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -44,47 +41,43 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
+    /**
+     * getAllPicture : Récupère toutes les images du téléphone et ajoute l'adresse dans imageList
+     */
     public void getAllPicture(){
 
-        Uri uri;
-        Cursor cursor;
-        int column_index;
-        String path;
-        String sortOrder;
+        Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String sortOrder = MediaStore.Images.ImageColumns.DATE_ADDED + " DESC";
         imageList = new ArrayList<>();
-        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
         String[] projection = {MediaStore.MediaColumns.DATA};
-        sortOrder = MediaStore.Images.ImageColumns.DATE_ADDED + " DESC";
-        cursor = getContentResolver().query(uri, projection, null, null, sortOrder);
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, sortOrder);
 
         try {
             if (null != cursor) {
-                column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
                 while (cursor.moveToNext()) {
-                    path = cursor.getString(column_index);
+                    String path = cursor.getString(column_index);
                     imageList.add(path);
                 }
                 cursor.close();
             }
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * onRequestPermissionsResult : Récupère les photos si la permission a été accordée
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
     public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults)
     {
-        switch (requestCode) {
-            case PICK_FROM_GALLERY:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getAllPicture();
-                } else {
-                }
-                break;
+        if (requestCode==PICK_FROM_GALLERY && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getAllPicture();
         }
     }
 
--- app/src/main/java/com/example/tpandroid/Touch.java
+++ app/src/main/java/com/example/tpandroid/Touch.java
@@ -6,86 +6,83 @@ import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+
 import java.util.ArrayList;
 
-public class Touch extends View {
 
+class Touch extends View {
+
+    // Echelle du zoom
     private float mScale = 1f;
-    private GestureDetector mGestureDetector;
+    // Echelle du zoom précédent
+    private float mScaleBefore = 1f;
+    // Nombre d'images par ligne
+    private int nbCols = 3;
+    // Chemin vers les images
+    private ArrayList<String> imageList;
+    // Evite les doubles zoom
+    private boolean alreadyTouched = false;
+
     private ScaleGestureDetector mScaleGestureDetector;
 
-    public class ZoomGesture extends GestureDetector.SimpleOnGestureListener {
-        private boolean normal = true;
-
-        @Override
-        public boolean onDoubleTap(MotionEvent e) {
-            mScale = normal ? 3f : 1f;
-            normal = !normal;
-            invalidate();
-            return true;
-        }
-    }
-
-
-    /////////////////////////////////////////////////////////////////////////////////
-
-
-    private float mScaleBefore = 1f;
-    private int nbCols = 3;
-
-
-    ArrayList<String> imageList;
 
     public Touch(Context context, ArrayList<String> imageList) {
-
         super(context);
         this.imageList = imageList;
-
-        mGestureDetector = new GestureDetector(context, new ZoomGesture());
         mScaleGestureDetector = new ScaleGestureDetector(context, new ScaleGesture());
     }
 
+    /**
+     * onDraw : Change le nombre d'images en fonction du zoom et les réaffiche
+     * @param canvas
+     */
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        int compteur = 0;
-        int widthPhone = getWidth();
-
         if (mScaleBefore - mScale < 0) {
-            // Agrandir
-            if (nbCols > 1)
-                nbCols--;
+            // Zoom
+            if (nbCols > 1) nbCols--;
         } else if (mScaleBefore - mScale > 0) {
-            // Retrécir
-            if (nbCols < 7)
-                nbCols++;
+            // Dézoom
+            if (nbCols < 7) nbCols++;
         }
-
         mScaleBefore = mScale;
+        afficherImages(canvas);
+    }
+
+    /**
+     * afficherImages : Affiche les images dans le canvas en fonction du nombre d'images par ligne
+     * (nbCols) et de la liste des images (imageList)
+     * @param canvas
+     */
+    private void afficherImages (Canvas canvas){
+
+        int widthPhone = getWidth();
+        int compteur = 0;
 
         if (imageList != null) {
 
             int i = 0, j = 0;
 
-            while (compteur < imageList.size() && compteur < 50) {
+            // Affiche un maximum de 8 images car le zoom ne fonctionne plus bien après
+            while (compteur < imageList.size() && compteur < 8) {
 
-
+                // Récupère les images par rapport au chemin
                 Drawable d1 = Drawable.createFromPath(imageList.get(i * nbCols + j));
                 Bitmap bitmap = ((BitmapDrawable) d1).getBitmap();
 
                 if (bitmap != null) {
+                    // On resize pour diminuer la taille en pixel des images
                     Drawable d = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap, 800 / nbCols, 800 / nbCols, true));
-
                     d.setBounds(widthPhone * j / nbCols, widthPhone * i / nbCols, widthPhone * (j + 1) / nbCols, widthPhone * (i + 1) / nbCols);
                     d.draw(canvas);
                 }
 
-
+                // On change les index des images à récupérer
                 j++;
                 if (j >= nbCols) {
                     i++;
@@ -97,29 +94,31 @@ public class Touch extends View {
         }
     }
 
-
-    private boolean alreadyTouched = false;
-
+    /**
+     * onTouchEvent : Appel la méthode de zoom si elle n'a pas déjà été appelée
+     * @param event
+     * @return
+     */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
 
         if (alreadyTouched){
             alreadyTouched = false;
         }else{
-            mGestureDetector.onTouchEvent(event);
+            // On appelle la méthode de zoom
             mScaleGestureDetector.onTouchEvent(event);
             alreadyTouched = true;
         }
         return true;
     }
 
-
+    /**
+     * ScaleGesture : Récupère l'échelle du zoom effectué
+     */
     public class ScaleGesture extends ScaleGestureDetector.SimpleOnScaleGestureListener {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             mScale *= detector.getScaleFactor();
-            Log.d("Coucou", "MScale : " + mScale);
             invalidate();
             return true;
         }
